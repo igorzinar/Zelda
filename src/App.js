872b3330d9814e848da523307b433afc61e44ff9
Igor Zinar <igorzinar@gmail.com>
2020-12-25 15:22:56 +0200

    initial project zelda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const canvasRef = useRef(null)
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+
+  // height and width of canvas
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d')
+    context.canvas.height = window.innerHeight
+    context.canvas.width = window.innerWidth
+
+    context.fillRect(x, y, 100, 100)
+  }, [])
+
+  // move the box when x or y changed
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d')
+    context.fillRect(x, y, 100, 100)
+  }, [x, y])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="app">
+      <canvas ref={canvasRef} />
 
-export default App;
+      <div className="arrows">
+        <button>Up</button>
+        <button>Left</button>
+        <button>Down</button>
+        <button onClick={() => setX((x) => x + 20)}>Right</button>
+      </div>
+
+      <div className="images">
+        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
+        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
+        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+      </div>
+    </div>
+  )
+}
